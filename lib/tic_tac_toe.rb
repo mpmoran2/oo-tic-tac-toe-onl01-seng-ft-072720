3927fe5e2f5937d59ea75e4e1b211ac38f08f2d6
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:00:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,4 +6,32 @@ end
 
 WIN_COMBINATIONS = [
   [0,1,2]
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
   ]
+  
+  def display_board
+  end 
+  
+  def input_to_index
+  end 
+  
+  def move
+  end 
+  
+  def position_taken?
+  end 
+  
+  def valid_move?
+  end 
+  
+  def turn 
+  end 
+  
+  
+  
