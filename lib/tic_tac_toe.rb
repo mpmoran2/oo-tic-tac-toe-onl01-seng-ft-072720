727eb64ce9ad33b724397f414956e34e7a586e5c
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:07:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,6 +47,16 @@ WIN_COMBINATIONS = [
   end 
   
   def turn 
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
   end 
   
   def turn_count
