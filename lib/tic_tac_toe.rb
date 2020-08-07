9fb49c2474405962199cb96026745c6dcb3bb344
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:04:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,13 +23,18 @@ WIN_COMBINATIONS = [
   ]
   
   def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end 
   
   def input_to_index(input)
     input.to_i - 1
   end 
   
-  def move
+  def move(position, char)
   end 
   
   def position_taken?
